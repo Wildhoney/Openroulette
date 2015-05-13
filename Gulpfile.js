614a054ee123cf33e3065db727113d185b76bf61
Wildhoney <adam.timberlake@gmail.com>
2015-05-13 22:37:21 +0100

    Trimmed gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -12,49 +12,7 @@
         argv         = require('yargs').argv,
         fs           = require('fs'),
         gulp         = require('gulp'),
-        concat       = require('gulp-concat'),
-        karma        = require('gulp-karma'),
-        sass         = require('gulp-sass'),
-        cssmin       = require('gulp-cssmin'),
-        jshint       = require('gulp-jshint'),
-        uglify       = require('gulp-uglify'),
-        processhtml  = require('gulp-processhtml'),
-        autoprefixer = require('gulp-autoprefixer');
-
-    /**
-     * @method eachVendorFile
-     * @param type {String}
-     * @return {Array}
-     */
-    var eachVendorFile = function eachVendorFile(type) {
-
-        var modulePaths = [],
-            modules     = config.dependencies[type] || [];
-
-        modules.forEach(function forEach(moduleName) {
-
-            if (moduleName.match(/\//i)) {
-
-                // Configuration actually provides relative path to the JavaScript file.
-                modulePaths.push(config.directories.vendor + '/' + moduleName);
-                return;
-
-            }
-
-            var directory = config.directories.vendor + '/' + moduleName,
-                bowerPath = directory + '/bower.json',
-                content   = fs.readFileSync(bowerPath, 'utf8'),
-                jsonDoc   = JSON.parse(content),
-                path      = directory + '/' + jsonDoc.main.replace(/^\.\//i, '');
-
-            // Voila!
-            modulePaths.push(path);
-
-        });
-
-        return modulePaths;
-
-    };
+        jshint       = require('gulp-jshint');
 
     /**
      * @property {Object} config
@@ -62,43 +20,7 @@
      */
     var config = yaml.safeLoad(fs.readFileSync(OPTIONS_PATH, 'utf8'));
 
-    /** Tasks... */
-
-    gulp.task('process-html', function processHtml() {
-
-        if (!argv.production) {
-
-            // Don't process this task unless we're compiling for the production
-            // environment, or an environment that is mimicking the production server,
-            // such as the test server.
-            return;
-
-        }
-
-        gulp.src(config.directories.public + '/' + config.documents.index)
-            .pipe(processhtml())
-            .pipe(gulp.dest(config.directories.public));
-
-    });
-
-    gulp.task('concat-all', ['compile-sass'], function minifyScripts() {
-
-        var scriptName = config.directories.vendor + '/' + config.name + '.min.js',
-            styleName  = config.directories.vendor + '/' + config.name + '.min.css';
-
-        gulp.src(eachVendorFile('scripts').concat(config.documents.scripts))
-            .pipe(concat({ path: scriptName, base: config.directories.vendor }))
-            .pipe(uglify())
-            .pipe(gulp.dest(config.directories.vendor + '/' + config.name));
-
-        gulp.src(eachVendorFile('styles').concat(config.documents.styles))
-            .pipe(concat({ path: styleName, base: config.directories.vendor }))
-            .pipe(cssmin())
-            .pipe(gulp.dest(config.directories.vendor + '/' + config.name));
-
-    });
-
-    gulp.task('js-hint', function jsHint() {
+    gulp.task('lint', function() {
 
         return gulp.src(config.documents.scripts)
                    .pipe(jshint())
@@ -106,35 +28,7 @@
 
     });
 
-    gulp.task('compile-sass', function compileSass() {
-
-        return gulp.src(config.directories.public + '/sass/default.scss')
-                   .pipe(sass())
-                   .pipe(autoprefixer({
-                       browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']
-                   }))
-                   .pipe(gulp.dest(config.directories.public + '/css'));
-
-    });
-
-    gulp.task('karma-tests', function karmaTests() {
-
-        return gulp.src([])
-                   .pipe(karma({
-                       configFile: 'karma.conf.js',
-                       action: 'run'
-                   }))
-                   .on('error', function onError(error) {
-                       throw error;
-                   });
-
-    });
-
-    gulp.task('test', ['js-hint', 'karma-tests']);
-    gulp.task('build', ['compile-sass', 'concat-all', 'process-html']);
+    gulp.task('test', ['lint']);
     gulp.task('default', ['test', 'build']);
-    gulp.task('watch', function watch() {
-        gulp.watch(['public/sass/*', 'public/sass/**/*'], ['compile-sass']);
-    });
 
 })();
